72ea6557ed5b424c7bc1a2149842f0c7d46b553c
Douglas254 <dobara@kabarak.ac.ke>
2022-03-04 22:45:05 +0300

    use of usestate in creating readmore toggler

--- learn-reactjs/src/App.js
+++ learn-reactjs/src/App.js
@@ -1,29 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import User from "./User";
+import Content from "./components/ReadMore";
 
 function App() {
   return (
     <div className="App">
-      {/* component can be created once and be reused many times i.e the User component that we have */}
-      <User userName="Douglas" tweet="Great learning react" />
-      <User userName="Basil" tweet="Python Evangelist" />
-      <User userName="Ben" tweet="Java Guru" />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Content />
     </div>
   );
 }
