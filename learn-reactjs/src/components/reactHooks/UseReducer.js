1c453e1a1bc00288ba8d010aed5635df99b089db
Douglas254 <dobara@kabarak.ac.ke>
2022-03-14 20:00:11 +0300

    useReducer Hook

--- learn-reactjs/src/components/reactHooks/UseReducer.js
+++ learn-reactjs/src/components/reactHooks/UseReducer.js
@@ -1,21 +1,33 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+
+    case "toggleShowText":
+      return { count: state.count, showText: !state.showText };
+
+    default:
+      return state;
+  }
+};
 
 function UseReducer() {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button
         onClick={() => {
-          setCount(count + 1);
-          setShowText(!showText);
+          dispatch({ type: "INCREMENT" });
+          dispatch({ type: "toggleShowText" });
         }}
       >
         Click Here
       </button>
       {/* <p>...</p> tag only appears when the boolean showText = true  */}
-      {showText && (
+      {state.showText && (
         <b>
           <p>This is a Text</p>
         </b>
